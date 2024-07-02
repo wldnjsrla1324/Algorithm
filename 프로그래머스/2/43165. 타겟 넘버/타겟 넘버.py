2232fad8c39b06d1afece1c3e3cb1ef2909a2d82
Jiwon Kim <141472393+wldnjsrla1324@users.noreply.github.com>
2024-07-02 22:58:05 +0900

    [level 2] Title: 타겟 넘버, Time: 5.32 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,19 @@
+def solution(numbers, target):
+    answer = 0
+    total = 0
+    
+    def dfs(i,total,n_len):
+        i += 1
+        nonlocal answer
+        if i >= n_len:
+            if total == target:
+                answer += 1
+            else:
+                return False
+        else:
+            dfs(i,total+numbers[i],n_len)
+            dfs(i,total-numbers[i],n_len)
+        
+    dfs(-1,total,len(numbers))
+    
+    return answer
