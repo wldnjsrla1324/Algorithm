a50a44163b35eb09f509910497bbd9122c2e9f70
Jiwon Kim <141472393+wldnjsrla1324@users.noreply.github.com>
2024-07-03 17:49:42 +0900

    [level 3] Title: 네트워크, Time: 0.23 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,25 @@
+def solution(n, computers):
+    # dfs 탐색
+    def dfs(i, visited, computers):
+        visited[i] = True #방문 컴퓨터는 방문 처리
+        for j in range(n):
+            if computers[i][j] == 1 and visited[j]==False:
+                dfs(j, visited, computers)
+
+    visited = [False]*n #초기 방문여부 세팅
+    answer = 0
+    for i in range(n):
+        if not visited[i]:
+            dfs(i, visited, computers)
+            answer +=1 #dfs 탐색 종료하면 네트워크 하나 추가
+    return answer
+
+#case1
+#110
+#110
+#001
+
+#case2
+#110
+#111
+#011
