5c00fdbae399f407718fd19996e83ab0fd1172f8
Jiwon Kim <141472393+wldnjsrla1324@users.noreply.github.com>
2024-07-10 20:20:25 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,11 +1,15 @@
 def solution(array, commands):
     answer = []
-    for c in commands:
-        i = c[0]
-        j = c[1]
-        k = c[2]
-        arr = array[i-1:j]
-        arr.sort()
-        answer.append(arr[k-1])
+
+    for i in range(len(commands)):
+        start = commands[i][0] #i
+        end = commands[i][1] #j
+        k = commands[i][2] #k
+        
+        arr = array[start-1:end] # 리스트 슬라이스
+        
+        #정렬
+        arr = sorted(arr)
+        answer.append(arr[k-1]) 
         
     return answer
