c064ddf17fdeac7a0b3b8adb8332e193bd80f444
Jiwon Kim <141472393+wldnjsrla1324@users.noreply.github.com>
2024-07-26 15:27:14 +0900

    [level 1] Title: 모의고사, Time: 3.44 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,16 +1,14 @@
 def solution(answers):
-    answer = [0,0,0]
-    #최소공배수 길이만큼 설정
-    p1 = [1,2,3,4,5]*8
-    p2 = [2,1,2,3,2,4,2,5]*5
-    p3 = [3,3,1,1,2,2,4,4,5,5]*4
-    #정답이면 1씩 추가
+    answer=[0,0,0]
+    p1=[1,2,3,4,5] 
+    p2=[2,1,2,3,2,4,2,5]
+    p3=[3,3,1,1,2,2,4,4,5,5]
     for i in range(len(answers)):
-        if answers[i] == p1[i%40]:
+        if answers[i]==p1[i%len(p1)]:
             answer[0] +=1
-        if answers[i] == p2[i%40]:
+        if answers[i]==p2[i%len(p2)]:
             answer[1] +=1
-        if answers[i] == p3[i%40]:
+        if answers[i]==p3[i%len(p3)]:
             answer[2] +=1
     max_score=max(answer)
-    return [i+1 for i, score in enumerate(answer) if score == max_score]
+    return [i+1 for i, score in enumerate(answer) if score==max_score]
