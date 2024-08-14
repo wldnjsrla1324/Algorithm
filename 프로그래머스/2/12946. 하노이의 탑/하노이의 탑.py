618c23deccc192c78ebde77cfa7a3efa5eb20ca8
Jiwon Kim <141472393+wldnjsrla1324@users.noreply.github.com>
2024-08-14 14:50:08 +0900

    [level 2] Title: 하노이의 탑, Time: 10.52 ms, Memory: 18.7 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,15 +1,16 @@
 def solution(n):
     answer = []
     
-    def hanoi(n, start, end, inter):
+    def hanoi(n, start, inter, end):
         if n==1:
             answer.append([start,end])
+        
         else:
-            hanoi(n-1, start, inter, end)
-            hanoi(1, start, end, inter)
-            hanoi(n-1, inter, end, start)
+            hanoi(n-1, start, end, inter)
+            hanoi(1, start, inter, end)
+            hanoi(n-1, inter, start, end)
     
-    hanoi(n, 1, 3, 2)
+    hanoi(n, 1, 2, 3)
     return answer
 
 
