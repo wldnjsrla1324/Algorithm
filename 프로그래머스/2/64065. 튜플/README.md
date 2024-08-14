e22ac730576b7ce692ec679674c88e31ebbd9a50
Jiwon Kim <141472393+wldnjsrla1324@users.noreply.github.com>
2024-08-14 15:39:06 +0900

    [level 2] Title: 튜플, Time: 47.35 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/README.md
+++ 프로그래머스/2/64065. 튜플/README.md
@@ -1,10 +1,10 @@
 # [level 2] 튜플 - 64065 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065) 
 
 ### 성능 요약
 
-메모리: 12 MB, 시간: 25.89 ms
+메모리: 12.1 MB, 시간: 47.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 08일 18:41:34
+2024년 08월 14일 15:39:05
 
 ### 문제 설명
 
--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,13 +1,14 @@
 def solution(s):
     result = []
     answer = []
-    s_split = list(s[2:-2].split('},{'))
-    #print(s_split)
-    s_split.sort(key = lambda x:len(x))
-    #print(s_split)
-    for sp in s_split:
-        temp = set(list(map(int,sp.split(','))))
+    
+    answer= list(s[2:-2].split('},{'))
+    answer.sort(key = lambda x:len(x))
+    temp={}
+    for a in answer:
+        a = set(list(map(int,a.split(','))))
         
-        result = result + list(set.difference(temp,answer))
-        answer=temp
+        result.append(list(set.difference(a,temp))[0])
+        
+        temp = a
     return result
