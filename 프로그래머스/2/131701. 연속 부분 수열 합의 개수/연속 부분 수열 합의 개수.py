a0904e7d035a9a6d4d37d1c1286520bce6e69bb5
Jiwon Kim <141472393+wldnjsrla1324@users.noreply.github.com>
2024-08-18 18:58:24 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4234.49 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,17 +1,15 @@
-from itertools import permutations, combinations
 def solution(elements):
     answer = 0
-    ele = ''
-    s = set()
+    s=set()
     
     l = len(elements)
     
     elements+=elements
-    print(elements)
     
-    for i in range(l) : 
-        for j in range(l) : 
+    
+    for i in range(l):
+        for j in range(l):
             s.add(sum(elements[j:j+i+1]))
-            
+    
         
     return len(s)
