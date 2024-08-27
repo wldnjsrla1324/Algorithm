eabaa0df560d24e984b73b9fb8f35650d0daa496
Jiwon Kim <141472393+wldnjsrla1324@users.noreply.github.com>
2024-08-27 21:56:56 +0900

    [level 3] Title: 가장 먼 노드, Time: 7342.46 ms, Memory: 19.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,25 @@
+from collections import deque
+def solution(n, edge):
+    answer = 0
+    graph =[[] for _ in range(n+1)]
+    distance = [-1] * (n+1)
+    
+    for e in edge :
+        graph[e[0]].append(e[1])
+        graph[e[1]].append(e[0])  
+    
+    queue = deque([1])
+    distance[1]=0 
+    
+    while queue :
+        now = queue.popleft() 
+        
+        for i in graph[now]:
+            if distance[i] == -1: 
+                queue.append(i) 
+                distance[i] = distance[now] + 1
+    
+    for d in distance:
+        if d == max(distance):
+            answer += 1
+    return answer
