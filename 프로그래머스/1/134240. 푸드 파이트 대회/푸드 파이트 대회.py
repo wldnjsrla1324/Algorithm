7e5be18349d70260b515185fb1d5d960f9d0a246
Jiwon Kim <141472393+wldnjsrla1324@users.noreply.github.com>
2024-08-28 14:09:58 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.88 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -2,12 +2,12 @@ def solution(food):
     
     answer=''
     
-    for i in range(1,len(food)):
-        if food[i]>1:
-            for j in range(food[i]//2):
-                answer+=str(i)
-    r=reversed(answer)
-    r=''.join(r)
+    for i in range(1, len(food)):
+        for j in range(food[i]//2):
+            answer+=str(i)
+    
+    r = ''.join(reversed(answer))
+    
     answer+='0'
     answer+=r
     return answer
